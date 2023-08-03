098786a2d56aff53e685cf2f00a010b8d926863a
crazyjncsu <j@thewolfweb.com>
2023-08-03 13:37:58 -0400

    stuff

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,4 +1,4 @@
-smartthings edge:drivers:package AutoBridge/
+smartthings edge:drivers:package edge/AutoBridge/
 smartthings edge:channels:assign 40bf6cb7-929e-4b26-b698-a1e5d3f2c06f --channel=68b51e84-9d0e-4729-80ba-8d929bc24205
 smartthings edge:drivers:install 40bf6cb7-929e-4b26-b698-a1e5d3f2c06f --channel=68b51e84-9d0e-4729-80ba-8d929bc24205 --hub=0cd0ea5d-015f-45c1-8d69-226a596d732a
 #smartthings edge:drivers:install 40bf6cb7-929e-4b26-b698-a1e5d3f2c06f --channel=68b51e84-9d0e-4729-80ba-8d929bc24205 --hub=14c621a2-5672-4d13-888e-3db17a1a0e98
---
+++ logcat.sh
@@ -0,0 +1,1 @@
+smartthings edge:drivers:logcat 40bf6cb7-929e-4b26-b698-a1e5d3f2c06f --hub-address=192.168.1.65
